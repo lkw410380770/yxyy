56eba6dc9ef3313c6800666609e7efd58a6e975a
lkw410380 <410380770@qq.com>
2017-06-29 14:04:33 +0800

    修改头部

---
+++ js/public.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    var oBtn = document.querySelector('.clickEle');
+    var oBox = document.querySelector('.slide-nav');
+    var oMask = document.querySelector('.mask');
+    oBtn.onclick = function() {
+        oBox.style.right = 0;
+        oMask.style.display = 'block';
+        oMask.style.opacity = 1;
+    }
+    oMask.onclick = function() {
+        oBox.style.right = -20 + '%';
+        this.style.opacity = 0;
+        this.style.display = 'none;'
+    }
+}
